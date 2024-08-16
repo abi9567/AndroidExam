3e2f3178b7624f3a084163f4828de9179d541064
Abi <abicherayakkattu2012@gmail.com>
2024-08-16 12:37:24 +0530

    change fab icon color

--- app/src/main/java/com/example/androidexam/ui/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/example/androidexam/ui/homeScreen/HomeScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -30,13 +29,14 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.androidexam.ui.homeScreen.homeScreenComponents.BottomSheetView
 import com.example.androidexam.ui.homeScreen.homeScreenComponents.HomeBannerSliderView
 import com.example.androidexam.ui.homeScreen.homeScreenComponents.ListItemSingleView
 import com.example.androidexam.ui.homeScreen.homeScreenComponents.SearchView
-import com.example.androidexam.ui.theme.DotColor
+import com.example.androidexam.ui.theme.FabIconColor
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
@@ -93,7 +93,7 @@ fun HomeScreen(modifier : Modifier = Modifier,
                 .padding(all = 16.dp)
                 .clip(shape = CircleShape)
                 .align(alignment = Alignment.BottomEnd)
-                .background(color = DotColor),
+                .background(color = FabIconColor),
                 onClick = {
                     viewModel.getTopThreeCharacterCount()
                     scope.launch { bottomSheetState.show() }
